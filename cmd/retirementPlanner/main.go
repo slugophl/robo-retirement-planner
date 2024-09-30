97fe5b6a2077dabca744a476f07ad4cdbe601b70
John Laffey <laffeychem@gmail.com>
2024-09-30 16:58:55 -0400

    add some validation rebuild binaries

--- cmd/retirementPlanner/main.go
+++ cmd/retirementPlanner/main.go
@@ -3,24 +3,28 @@ package main
 import "fmt"
 
 func main() {
+
+	var spouseWorking bool
 	var spouseIncome float64
 	for {
 
 		fmt.Println("Welcome to retirement planning advisor, please listen to the following prompts.")
 		age := getValue("Please enter your age: ")
 		currentIncome := getValue("What is your current income?: ")
-		ageToRetire := getValue("At what age do you want to retire?: ")
+		ageToRetire := getRetirementAge("At what age do you want to retire?: ", age)
 		marritalStatus := getBoolValue("Are you married? Please enter y or n: ")
-		spouseWorking := getBoolValue("Is your spouse working? Please enter y or n: ")
-		if spouseWorking {
-			spouseIncome = getValue("Please enter your spouse's income: ")
+		if marritalStatus {
+			spouseWorking = getBoolValue("Is your spouse working? Please enter y or n: ")
+			if spouseWorking {
+				spouseIncome = getValue("Please enter your spouse's income: ")
+			}
 		}
 		numOfKids := getKids("How many kids do you have or are you planning to have?: ")
 		retirementTargetAmount := getValue("What is the target value you want for your savings?: ")
 		houseOwnership := getBoolValue("Do you own a house or plan to buy one? Please enter y or n: ")
 
 		fmt.Printf(
-			"Your age is %.2f\nYour current income is %.2f\nYour target retirement age is %.2f\nYour marital status is %t\nIs your spouse working? %t\nYour spouse's income is %.2f\nNumber of kids: %.2f\nYour target retirement amount is %.2f\nDo you own a house?: %t\n",
+			"Your age is %.0f\nYour current income is $%.2f\nYour target retirement age is %.0f\nYour marital status is %t\nIs your spouse working? %t\nYour spouse's income is $%.2f\nNumber of kids: %.0f\nYour target retirement amount is %.2f\nDo you own a house?: %t\n",
 			age, currentIncome, ageToRetire, marritalStatus, spouseWorking, spouseIncome, numOfKids, retirementTargetAmount, houseOwnership)
 		return
 	}
@@ -30,7 +34,11 @@ func getValue(text string) float64 {
 	for {
 		var userInput float64
 		fmt.Print(text)
-		fmt.Scan(&userInput)
+		_, err := fmt.Scanln(&userInput)
+		if err != nil {
+			fmt.Println("Please enter a positive number that is greater than 0.")
+			continue
+		}
 		if userInput > 0 {
 			return userInput
 		} else {
@@ -44,14 +52,15 @@ func getBoolValue(text string) bool {
 	for {
 		var userInput string
 		fmt.Print(text)
-		fmt.Scan(&userInput)
+		_, err := fmt.Scanln(&userInput)
+		if err != nil {
+			fmt.Println("Please enter either y for yes, or n for no")
+			continue
+		}
 		if userInput == "y" || userInput == "Y" {
 			return true
 		} else if userInput == "n" || userInput == "N" {
 			return false
-		} else {
-			fmt.Println("Please enter either y for yes, or n for no")
-			continue
 		}
 	}
 }
@@ -60,9 +69,32 @@ func getKids(text string) float64 {
 	for {
 		var userInput float64
 		fmt.Print(text)
-		fmt.Scan(&userInput)
-		if userInput < 0 || userInput > 50 {
-			fmt.Println("Please enter a positive number that is less than 50 or greater than 0.")
+		_, err := fmt.Scanln(&userInput)
+		if err != nil {
+			fmt.Println("Please enter a positive number that is less than 26 or greater than 0.")
+			continue
+		}
+		if userInput > 0 || userInput <= 25 {
+			return userInput
+		} else {
+			fmt.Println("Please enter a positive number that is less than 26 or greater than 0.")
+			continue
+
+		}
+	}
+}
+
+func getRetirementAge(text string, age float64) float64 {
+	for {
+		var userInput float64
+		fmt.Print(text)
+		_, err := fmt.Scanln(&userInput)
+		if err != nil {
+			fmt.Printf("Please enter a positive number that is greater than %.0f\n", age)
+			continue
+		}
+		if userInput <= age {
+			fmt.Printf("Please enter an age greater than %.0f\n", age)
 			continue
 		} else {
 			return userInput
